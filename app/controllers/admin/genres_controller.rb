4c130ea7b74ed780799e057bd99b11479558c765
KazukiUeno <153255954+Kazuki412@users.noreply.github.com>
2024-02-16 15:37:04 +0000

    残りは編集を反映させる

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,2 +1,28 @@
 class Admin::GenresController < ApplicationController
+
+  def create
+    @genre = Genre.new(genre_params)
+    @genre.save
+    redirect_to admin_genres_path
+  end
+
+  def index
+    @genres = Genre.all
+  end
+
+  def edit
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    @genre.update
+    redirect_to admin_genres_path
+  end
+
+  private
+
+  def genre_params
+    params.permit(:name)
+  end
+
 end
