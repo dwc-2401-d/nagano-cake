0031c8737923b797bfc437df46160f44e2ba166d
EC2 Default User <takizawasayuki0912@gmail.com>
2024-02-17 07:13:22 +0000

    mergeにより最新状態

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,13 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :genres, only: [:create, :index, :edit, :update]
   end
+  
+  scope module: :public do
+    get "customers/my_page" => "customers#show"
+    get "customers/information/edit" => "customers#edit"
+    patch "customers/information" => "customers#update"
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users
 end
