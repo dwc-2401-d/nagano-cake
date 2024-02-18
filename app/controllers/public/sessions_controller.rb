967514ca358ad6f21eadb3b7b2d51a0193a17060
EC2 Default User <153484664+takuro-kamito@users.noreply.github.com>
2024-02-18 09:25:09 +0000

    ログイン画面コントローラー修正

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,7 +2,20 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :customr_state, only: [:create]
+  
+  
+  # deviceのアクション機能がデフォルトで入っている為、new,creteアクションの記述なし
+  # before_actionメソッドにて定義された時createだけアクション
+  
+  
+  def after_sign_in_path_for(resource)
+    public_top_path
+  end
 
+  def after_sign_out_path_for(resource)
+    public_top_path
+  end
   # GET /resource/sign_in
   # def new
   #   super
@@ -17,6 +30,24 @@ class Public::SessionsController < Devise::SessionsController
   # def destroy
   #   super
   # end
+
+#会員の登録の際の会員確認のメソッド
+# customerが入力したmail,passworが一致しているかの確認
+# 会員ではない入力者へのメッセージ
+# リダイレクトで新規会員登録ページ（new)へ移動する
+
+  private
+  
+  def customr_state
+    customer = Customer.find_by(email: params[:customer][:email])
+    return if customer.nil?
+    return unless customer.valid_password?(params[:customer][:password])
+    unless customer.is_active
+      flash[notice] = "退会されています"
+      redirect_to new_customeer_registration_path
+    end
+  end
+
 
   # protected
 
