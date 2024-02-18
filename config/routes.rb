9ef04f9709291ae6281f4986b1b4e058439ba9c0
KazukiUeno <153255954+Kazuki412@users.noreply.github.com>
2024-02-18 06:24:47 +0000

    避難

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   resources :customers, only: [:new, :create]
    end
 
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users
 end
