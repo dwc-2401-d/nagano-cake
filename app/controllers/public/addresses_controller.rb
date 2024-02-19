b9d00df162fd657ddc3d6b5d710b7115bfd54a8d
EC2 Default User <takizawasayuki0912@gmail.com>
2024-02-19 05:46:29 +0000

    コントローラ追記

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,2 +1,37 @@
 class Public::AddressesController < ApplicationController
+
+  def index
+    @addressed = Address.all
+    @address = Address.new
+  end
+
+  def create
+    @address = Address.new(address_params)
+    @address.save
+    redirect_to customers_addresses_path
+  end
+
+  def edit
+    @address = Address.find(params[:id])
+  end
+
+  def update
+    @address = Address.find(params[:id])
+    @address.update(address_params)
+    redirect_to customers_addresses_path
+  end
+
+  def destroy
+     @address = Address.find(params[:id])
+     @address.destroy
+     redirect_to customers_addresses_path
+  end
+
+
+  private
+
+  def address_params
+    params.require(:address).permit(:post_code, :address, :name)
+  end
+
 end
