7f358c8697e02ed3ae0d6589601e7edd27b430c0
EC2 Default User <153484664+takuro-kamito@users.noreply.github.com>
2024-02-19 01:48:24 +0000

    a

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,22 @@ class Item < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :order_details, dependent: :destroy
   belongs_to :genre
+
+  # 消費税計算
+
+  def add_tax_price
+    (self.price*1.10).round
+  end
+
+  # 商品一覧に関わるバリテーションの設定
+  # 商品名　紹介文　価格　写真
+
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :price, presence: true
+  validates :image, presence: true
+
+
+
+
 end
