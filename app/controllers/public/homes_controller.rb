122878c5fca65c66f05eec6913543abf47f5ad1d
EC2 Default User <153484664+takuro-kamito@users.noreply.github.com>
2024-02-20 02:46:44 +0000

    top

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,8 @@
 class Public::HomesController < ApplicationController
   def top
+    # （'id DESC').limit(4)idの降順にソートし、上位4つのレコードを取得するためのコード
     @items = Item.all
-    @genres = Genre.where(valid_invalid_status: 0)
+    @genres = Genre.all
     @items = Item.order('id DESC').limit(4)
   end
   
