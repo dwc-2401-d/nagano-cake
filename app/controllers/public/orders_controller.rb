9e5c44bc2497c08d43ec56d603c30e8ed69cb113
EC2 Default User <takizawasayuki0912@gmail.com>
2024-02-21 09:16:07 +0000

    confirm修正

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -2,6 +2,7 @@ class Public::OrdersController < ApplicationController
 
   def new
     @order = Order.new
+    @addresses = Address.all
   end
 
   def confirm
@@ -12,18 +13,18 @@ class Public::OrdersController < ApplicationController
     @cart_items_price = @cart_items.sum { |cart_item| cart_item.item.price * cart_item.amount }  # 商品金額合計
     @total_price = @shipping_cost + @cart_items_price  # 請求金額
 
-    @address_type = params[:order][:address_id]
+    @address_type = params[:order][:select_address]
     # 配送先の分岐
     case @address_type
     when "customer_address"
       @order.post_code = current_customer.post_code
       @order.address = current_customer.address
-      @order.neme = current_customer.first_name + current_customer.last_name
+      @order.name = current_customer.first_name + current_customer.last_name
     when "registered_address"
       @address = Address.find(params[:order][:address_id])
       @order.post_code = @address.post_code
       @order.address = @address.address
-      @order.neme = @address.name
+      @order.name = @address.name
     when "new_address"
       @order.customer_id = current_customer.id
     end
@@ -40,10 +41,10 @@ class Public::OrdersController < ApplicationController
     # OrderDetaolに渡す値を設定
     @cart_items.each do |cart_item|
       @order_details = OrderDetail.new
-      @order_details.order_id = order_id
-      @order_details.item_id = cart_item.item_id
+      @order_details.order_id = order.id
+      @order_details.item_id = cart_item.item.id
       @order_details.price = cart_item.item.with_tax_price
-      @order_details.amount = cart_item.amaount
+      @order_details.amount = cart_item.amount
       @order_details.making_status = 0
       @order_details.save!
     end
@@ -64,6 +65,6 @@ class Public::OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:customer_id, :post_code, :address, :name, :shipping_cost, :total_payment, :payment_method, :status)
+    params.require(:order).permit(:customer_id, :payment_method, :post_code, :address, :name, :total_price, :shipping_cost, :status)
   end
 end
