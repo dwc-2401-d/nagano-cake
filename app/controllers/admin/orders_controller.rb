328d0933f991f22ed25e20aa5a604cc62ae5443a
KazukiUeno <153255954+Kazuki412@users.noreply.github.com>
2024-02-23 08:59:37 +0000

    エラー出てます。

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,32 +1,38 @@
 class Admin::OrdersController < ApplicationController
-　before_action :authenticate_admin 
-　# 管理者でないユーザーが不正な操作を行うことを防ぐ
-　
+  before_action :authenticate_admin!
+  # 管理者でないユーザーが不正な操作を行うことを防ぐ
+
   def show
     @order = Order.find(params[:id])
     @order_details = @order.order_details
+    @shipping_cost = 800  # 送料は固定
+    @order_product_total = 0
+    @order_details.each do |order_detail|
+      order_total = order_detail.price * order_detail.amount
+      @order_product_total += order_total
+    end
   end
-  
+
   # 条件分岐でorder.status == "confirm_paymentの条件がtrueの場合
-　# making_status: "waiting_manufacture")`が実行される
-　# making_status側が"waiting_manufacture"に変更されます
+  # making_status: "waiting_manufacture")`が実行される
+  # making_status側が"waiting_manufacture"に変更されます
 
-　
+
   def update
-    order = Order.find(params[:id])
-    order_details = order.order_details
-    order.update(order_params)
+    @order = Order.find(params[:id])
+    @order.update(status: params[:order][:status])
+    order_details = @order.order_details
 
-    if order.status == "confirm_payment"
+    if @order.status == "confirm_payment"
       order_details.update_all(making_status: "waiting_manufacture")
     end
 
-    redirect_to admin_order_path(order.id)
+    redirect_to admin_order_path(@order)
   end
 
   private
 
   def order_params
-    params.require(:order).permit(:status)
+    params.require(:order).permit(:customer_id, :name, :post_code, :address, :payment_method, :status)
   end
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,14 +1,9 @@
 class OrderDetail < ApplicationRecord
   belongs_to :order
   belongs_to :item
-  
+
   # 要件定義　making status 制作不可:0 制作待ち:1 制作完了:2 config/locales/ja.ymlにも同様の記述
   # editにwaiting_manufacturreの同様の記述
-  
-  enum making_status: { 
-    impossible: 0, 
-    waiting_manufacturre: 1, 
-    finish: 2,
-    
-  }
+
+  enum making_status: { impossible: 0, waiting_manufacture: 1, in_making: 2,finish: 3 }
 end
