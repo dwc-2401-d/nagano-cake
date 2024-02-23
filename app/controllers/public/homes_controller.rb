e7f4a33e7c165254804fbc8516e09366b94f7fa5
KazukiUeno <153255954+Kazuki412@users.noreply.github.com>
2024-02-23 05:38:52 +0000

    a

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,13 @@
 class Public::HomesController < ApplicationController
+  
   def top
+    @items = Item.order('id DESC').limit(4)
   end
+  
+  def genre
+    @genres = Genre.all
+  end
+  
   def about
   end
 
