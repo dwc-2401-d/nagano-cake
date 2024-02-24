a3c68fd8af09d0a46491318136545953c4c1c259
EC2 Default User <takizawasayuki0912@gmail.com>
2024-02-24 02:38:21 +0000

    コンフリクト後追記

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -30,4 +30,21 @@ class Public::SessionsController < Devise::SessionsController
     root_path
   end
 
+
+  protected
+
+  # アクティブであることを判断する処理
+  def customer_state
+    @customer = Customer.find_by(email: params[:customer][:email])  # 1,入力されたemailからアカウントを１件取得
+    return if @customer.nil?  # 2,アカウントを取得できなかった場合、メソッドを終了
+    return unless @customer.valid_password?(params[:customer][:password])  # 3,取得したアカウントのパスワードが一致していない場合、メソッドを終了
+    if @customer.is_active  # 4,アクティブでない会員に対する処理
+      sign_in(@customer)
+      redirect_to root_path
+    else
+      flash[:notice] = "既に退会済みのアカウントです。新規会員登録が必要になります。"
+      redirect_to new_customer_registration_path
+    end
+  end
+
 end
