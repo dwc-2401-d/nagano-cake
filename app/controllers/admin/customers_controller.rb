58f4efb6c399c35ab1e8ada54a85a126c1e59e58
EC2 Default User <153484664+takuro-kamito@users.noreply.github.com>
2024-02-24 10:02:31 +0000

    ページネーション作成

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,26 +1,27 @@
 class Admin::CustomersController < ApplicationController
 # 管理者以外はアクセスできないように
+
   def index
-    @customers = Customer.all
-  end 
-  
+   @customers = Customer.all.page(params[:page]).per(10)
+  end
+
   def show
     @customer = Customer.find(params[:id])
-  end 
-  
+  end
+
   def edit
     @customer = Customer.find(params[:id])
-  end 
-  
+  end
+
   def update # ifで実行できなかった時の動き
     @customer = Customer.find(params[:id])
     @customer.update(customer_params)
     redirect_to admin_customer_path(@customer)
-  end 
-  
+  end
+
   private
-  
+
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :first_name_kana, :last_name_kana, :post_code, :address, :telephone_number, :email, :is_active)
   end
-end
+  end
